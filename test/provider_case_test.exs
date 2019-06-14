24b2b0da78980bc2df86bbf2cc48e7230128f43c
Matt Glover <mpglover@gmail.com>
2019-06-14 16:09:17 -0400

    Adjust tests to pass for v1 example pact

--- test/provider_case_test.exs
+++ test/provider_case_test.exs
@@ -12,11 +12,12 @@ defmodule ProviderCaseTest do
   honours_pact_with("Alice service", "Consumer", "./test/pacts/v1-example-pact.json")
 
   defp mock_webserver(bypass) do
-    Bypass.stub bypass, "GET", "/provider.json", fn conn ->
+    Bypass.stub(bypass, "GET", "/mallory", fn conn ->
+      %{"name" => "ron", "status" => "good"} = URI.decode_query(conn.query_string)
+
       conn
-      |> Plug.Conn.resp(200, ~s<{"errors": [{"code": 88, "message": "Rate limit exceeded"}],"test": "NO","date": "2013-08-16T15:31:20+10:00", "count": 1000}>)
-      |> Plug.Conn.put_resp_content_type("application/json")
-      |> Plug.Conn.put_resp_header("x-extraneous", "extra headers")
-    end
+      |> Plug.Conn.resp(200, ~s("That is some good Mallory."))
+      |> Plug.Conn.put_resp_content_type("text/html")
+    end)
   end
 end
